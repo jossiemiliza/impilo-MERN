e147d61de9db00938f7e92f83c6e189b090a2820
jossiemiliza <josephine.miliza@gmail.com>
2021-09-13 14:42:07 +0300

    FRontend and Backend setup

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const connectDB = require("./config/db");
+const dotenv = require("dotenv");
+
+const app = express();
+dotenv.config();
+// connect database
+connectDB();
+
+// Init middleware
+app.use(express.json({ extended: false }));
+
+app.get("/", (req, res) => res.send("API Running"));
+
+// define routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/slots", require("./routes/api/slots"));
+app.use("/api/appointment", require("./routes/api/appointment"));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
